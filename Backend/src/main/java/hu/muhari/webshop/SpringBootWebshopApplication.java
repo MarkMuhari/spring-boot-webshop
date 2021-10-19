5671cc15048593350964a1970b28e014440a6bd0
Muhari Márk <markmuhari.eu@gmail.com>
2021-10-19 15:20:51 +0200

    first commit create new project

---
+++ Backend/src/main/java/hu/muhari/webshop/SpringBootWebshopApplication.java
@@ -0,0 +1,13 @@
+package hu.muhari.webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebshopApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/hu/muhari/webshop/SpringBootWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.muhari.webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
